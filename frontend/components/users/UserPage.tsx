c53d431add77268715995c2b6017434fad481bfc
me-safty <erensafty@gmail.com>
2023-08-19 09:27:13 +0300

    fix the streak in the user page

--- frontend/components/users/UserPage.tsx
+++ frontend/components/users/UserPage.tsx
@@ -1,15 +1,20 @@
 import { User } from "@/types"
 import UserHabits from "./UserHabits"
-import MiniCalender from "../habits/MiniCalender"
+// import MiniCalender from "../habits/MiniCalender"
 import UserBox from "./UserBox"
+import calcStreak from "@/lib/calcStreak"
 
 export default function UserPage({ user }: { user: User }) {
+  const habitsWithStreak = user.habits.map((habit) => ({
+    ...habit,
+    currentStreak: calcStreak(habit.dates),
+  }))
   return (
     <>
       {/* <MiniCalender /> */}
       <UserBox name={user.name} imglink={user.imglink} />
       {user.habits.length > 0 ? (
-        <UserHabits habits={user.habits} />
+        <UserHabits habits={habitsWithStreak} />
       ) : (
         <p className="text-white">no habits</p>
       )}
